c45147e97f05b5cb0b9219c97deb8666ce9ba3a4
Darkhan <didar3@mail.ru>
2021-08-27 02:34:19 +0600

    - AppDelegate and SceneDelegate are edited in order to be compatible starting from iOS 12

--- WeatherApp/Extra/AppDelegate.swift
+++ WeatherApp/Extra/AppDelegate.swift
@@ -8,24 +8,34 @@
 import UIKit
 import CoreData
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window : UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+            // do only pure app launch stuff, not interface stuff
+        } else {
+            self.window = UIWindow()
+            
+            let tabBar = TabBarController()
+            window?.rootViewController = tabBar
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
